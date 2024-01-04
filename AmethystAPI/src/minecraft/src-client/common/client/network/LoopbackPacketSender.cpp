277d115effc4faf0cb5895380d7349dbc50560e1
ATXLtheAxolotl <flashtube107@gmail.com>
2024-01-03 19:10:13 -0500

    optimize(): Use static when sig scanning to prevent rescans

--- AmethystAPI/src/minecraft/src-client/common/client/network/LoopbackPacketSender.cpp
+++ AmethystAPI/src/minecraft/src-client/common/client/network/LoopbackPacketSender.cpp
@@ -3,5 +3,6 @@
 
 void LoopbackPacketSender::sendToServer(Packet* packet) {
     using function = void(__thiscall*)(LoopbackPacketSender*, Packet*);
-    return reinterpret_cast<function>(SigScan("40 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 0F B6 41"))(this, packet);
+    static auto func = reinterpret_cast<function>(SigScan("40 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 0F B6 41"));
+    return func(this, packet);
 }
